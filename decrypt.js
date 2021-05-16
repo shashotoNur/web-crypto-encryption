9783b0a5a02445425dae77367799581258bc1947
Nurul Aziz <nurulaziz.nz@gmail.com>
2021-05-16 14:18:58 +0600

    crypto functions

---
+++ decrypt.js
@@ -0,0 +1,27 @@
+
+export default async (text, derivedKey) =>
+  {
+    try
+    {
+      const string = atob(text);
+      const uintArray = new Uint8Array(
+        [...string].map((char) => char.charCodeAt(0))
+      );
+      const algorithm =
+      {
+        name: "AES-GCM",
+        iv: new TextEncoder().encode("Initialization Vector"),
+      };
+      const decryptedData = await window.crypto.subtle.decrypt(
+        algorithm,
+        derivedKey,
+        uintArray
+      );
+
+      return new TextDecoder().decode(decryptedData);
+    }
+    catch (error)
+    {
+      return `Error decrypting message: ${error}`;
+    }
+  };
---
+++ deriveKey.js
@@ -0,0 +1,33 @@
+
+export default async (publicKeyJwk, privateKeyJwk) =>
+  {
+    const publicKey = await window.crypto.subtle.importKey(
+        "jwk",
+        publicKeyJwk,
+        {
+          name: "ECDH",
+          namedCurve: "P-256",
+        },
+        true,
+        []
+      );
+
+    const privateKey = await window.crypto.subtle.importKey(
+        "jwk",
+        privateKeyJwk,
+        {
+          name: "ECDH",
+          namedCurve: "P-256",
+        },
+        true,
+        ["deriveKey", "deriveBits"]
+      );
+
+    return await window.crypto.subtle.deriveKey(
+      { name: "ECDH", public: publicKey },
+      privateKey,
+      { name: "AES-GCM", length: 256 },
+      true,
+      ["encrypt", "decrypt"]
+    );
+  };
